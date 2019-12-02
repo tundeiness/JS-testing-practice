62d34edde67c7f8fd9424854703f192d784b7eba
pelzolga123 <pelzolga123@gmail.com>
2019-12-02 17:12:52 +0200

    Added calculator function and tests for it

---
+++ test/calculator.test.js
@@ -0,0 +1,17 @@
+const calculator = require('../src/calculator');
+
+test('returns result of add method', () => {
+  expect(calculator.add(2, 3)).toBe(5);
+});
+
+test('returns result of substract method', () => {
+  expect(calculator.substract(6, 4)).toBe(2);
+});
+  
+test('returns result of divide method', () => {
+  expect(calculator.divide(3, 2)).toBe(1.5);
+});
+
+test('returns result of add method', () => {
+  expect(calculator.multiply(3, 2)).toBe(6);
+});
